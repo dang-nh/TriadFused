774f0bd9b18a7247f3d762fb350ece48fbfd9fb8
dang-nh <dang.nh0407@gmail.com>
2025-08-20 19:56:34 +0700

    feat: add module stubs for future development
    - Create data module placeholder for dataset loaders (FUNSD, CORD, SROIE, DocVQA)
    - Create refine module placeholder for black-box optimization (NES, Bandit)
    - Establish extensible architecture for v2 feature additions
    - Document planned functionality in module docstrings

---
+++ triadfuse/data/__init__.py
@@ -0,0 +1,11 @@
+"""
+Dataset loaders for TriadFuse (TODO for v2)
+
+This module will contain loaders for:
+- FUNSD: Forms understanding
+- CORD: Receipt understanding
+- SROIE: Scanned receipts
+- DocVQA: Document visual question answering
+"""
+
+# TODO: Implement dataset loaders in v2
---
+++ triadfuse/refine/__init__.py
@@ -0,0 +1,10 @@
+"""
+Black-box refinement methods for TriadFuse (TODO for v2)
+
+This module will contain:
+- NES (Natural Evolution Strategies)
+- Bandit-based optimization
+- Query-efficient gradient estimation
+"""
+
+# TODO: Implement black-box refinement in v2
